f79cd3db6dce20cfe6b87f9d479382aef5696801
Andri Imanudin <andridelapanbelas@gmail.com>
2019-07-20 15:12:47 +0700

    delete .html

--- Novice/01-01/latihan/scripts/main.js
+++ Novice/01-01/latihan/scripts/main.js
@@ -1,2 +1,6 @@
-let myHeading = document.querySelector('h1');
-myHeading.textContent = 'Hello world!';
+let iceCream = 'chocolate';
+if(iceCream === 'chocolate') {
+  alert('Yay, I love chocolate ice cream!');    
+} else {
+  alert('Awwww, but chocolate is my favorite...');    
+}
