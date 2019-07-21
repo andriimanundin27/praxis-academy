1e8508c63147d79273204f7901ff8b51eade9a87
Andri Imanudin <andridelapanbelas@gmail.com>
2019-07-21 15:20:15 +0700

    add kasus

---
+++ Novice/01-02/latihan/dict.js
@@ -0,0 +1,9 @@
+let map = new Map();
+map.set('cat', 'kucing');
+map.set('dragon', 'naga');
+
+console.log(map.get("cat"))
+console.log(map.get("bird"))
+map.set('bird', 'burung')
+console.log(map.get("bird"))
+console.log(map.delete("bird"))
