07c0a516adaa3f626ae3d2340cf83f79e8b91bad
Andri Imanudin <andridelapanbelas@gmail.com>
2019-07-21 15:47:38 +0700

    create dict in web page

--- Novice/01-02/latihan/dict.js
+++ Novice/01-02/latihan/dict.js
@@ -7,3 +7,5 @@ console.log(map.get("bird"))
 map.set('bird', 'burung')
 console.log(map.get("bird"))
 console.log(map.delete("bird"))
+
+// u can see it http://lengkapin.000webhostapp.com/kamus.html
